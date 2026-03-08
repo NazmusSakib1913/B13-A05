479d6f8dc0bc712a1940e458a1b2ea7ff9e3dc70
NazmusSakib1913 <nsakib1913@gmail.com>
2026-03-08 16:54:43 +0600

    implement base GET request for issues list

--- js/app.js
+++ js/app.js
@@ -11,3 +11,16 @@ let allIssues = [];
 const grid = document.getElementById('issuesGrid');
 const loader = document.getElementById('loader');
 const issueCountLabel = document.getElementById('issueCount');
+
+async function fetchIssues() {
+    loader.classList.remove('hidden');
+    grid.innerHTML = '';
+    try {
+        const res = await fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues');
+        const data = await res.json();
+    } catch (error) {
+        console.error("Error:", error);
+    } finally {
+        loader.classList.add('hidden');
+    }
+}
