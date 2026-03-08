a6da1d1f24ca4df643c9e3b81227f962cfe02a27
NazmusSakib1913 <nsakib1913@gmail.com>
2026-03-08 16:51:57 +0600

    initialize global state and DOM elements

--- js/app.js
+++ js/app.js
@@ -6,3 +6,8 @@ document.getElementById('logoutBtn').addEventListener('click', () => {
     localStorage.removeItem('isLoggedIn');
     window.location.href = 'index.html';
 });
+
+let allIssues = [];
+const grid = document.getElementById('issuesGrid');
+const loader = document.getElementById('loader');
+const issueCountLabel = document.getElementById('issueCount');
